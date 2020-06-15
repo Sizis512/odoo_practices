1416a3ff64b0042ebebf1292a807053ecc46e833
Guillermo Murcia <guiller.512@gmail.com>
2020-06-15 14:16:01 +0200

    initial commit

---
+++ import_product_images/__manifest__.py
@@ -0,0 +1,7 @@
+{
+    'name': 'Import Product Images',
+    'description': 'Import images for your products from a ZIP folder',
+    'author': 'Guillermo Murcia',
+    'depends': [''],
+    'data': [''],
+}
