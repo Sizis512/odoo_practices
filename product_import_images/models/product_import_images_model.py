87e7c942c9e029d93bac4e2675e9693dc0d6f531
Guillermo Murcia <guiller.512@gmail.com>
2020-06-16 17:55:02 +0200

    started import logic and added fields to view

--- product_import_images/models/product_import_images_model.py
+++ product_import_images/models/product_import_images_model.py
@@ -1,8 +1,36 @@
-from odoo import models, fields, api
+from odoo import models, fields, api, exceptions
+import zipfile
+import random
 
 
 class ProductImportImages(models.TransientModel):
     _name = 'product.import.images'
     _description = 'Import Product Images'
 
-    folder_path = fields.Char()
+    products = fields.Many2many(
+            'product.template',
+            string='Products')
+    folder_path = fields.Char("Images folder path")
+    new_image = fields.Binary()
+
+    @api.model
+    def default_get(self, field_names):
+        selected = super(ProductImportImages, self).default_get(field_names)
+        selected['products'] = self.env.context['active_ids']
+        return selected
+
+    def do_import_images(self):
+        self.ensure_one()
+        zf = zipfile.ZipFile(self.folder_path)
+        name_list = zf.namelist()
+
+        for prod in self.products:
+            valid_names = []
+            for name in name_list:
+                if name[:-4] == prod.default_code:
+                    # TODO check if ends in .jpg or .png
+                    valid_names.append(name)
+            if valid_names:
+                image_name = random.choice(valid_names)
+                prod.write({'image': zf.read(image_name)})
+        return True
